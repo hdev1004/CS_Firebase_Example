58ef5719da09374cadf32aa114e80edb29b66a5d
Jin <H.dev@kakao.com>
2021-01-02 01:58:19 +0900

    commit

--- CsFirebaseBlog/Form1.cs
+++ CsFirebaseBlog/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,6 +25,21 @@ namespace CsFirebaseBlog
             Add_Document_with_AutoID();
         }
 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Add_Document_with_CustomID();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Add_Array();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Add_ListOfObjects();
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             string path = AppDomain.CurrentDomain.BaseDirectory + @"cs-firebase-blog-firebase-adminsdk-18cwe-b5971a4787.json";
@@ -46,7 +62,52 @@ namespace CsFirebaseBlog
             MessageBox.Show("data added successfully");
         }
 
-        
+        void Add_Document_with_CustomID()
+        {
+            DocumentReference DOC = db.Collection("Add_Document_Width_CustomID").Document("myDoc");
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"FirestName", "Kim" },
+                {"LastName","Jinwon" },
+                {"PhoneNumber", "010-1234-5789" }
+            };
+            DOC.SetAsync(data1);
+            MessageBox.Show("data added successfully");
+        }
+
+        void Add_Array()
+        {
+            DocumentReference DOC = db.Collection("Add_Aray").Document("myDoc");
+            Dictionary<string, object> data1 = new Dictionary<string, object>();
+
+            ArrayList array = new ArrayList();
+            array.Add(123);
+            array.Add("name");
+            array.Add(true);
+
+            data1.Add("My Array", array);
+            DOC.SetAsync(data1);
+
+            MessageBox.Show("data added successfully");
+        }
+
+        void Add_ListOfObjects()
+        {
+            DocumentReference DOC = db.Collection("Add_ListOfObjects").Document("myDoc");
+            Dictionary<string, object> MainData = new Dictionary<string, object>();
+            Dictionary<string, object> List1 = new Dictionary<string, object>()
+            {
+                {"FirestName", "Kim" },
+                {"LastName","Jinwon" },
+                {"PhoneNumber", "010-1234-5678" }
+            };
+
+            MainData.Add("MyList", List1);
+
+            DOC.SetAsync(MainData);
+            MessageBox.Show("data added successfully");
+        }
+
     }
 }
 
