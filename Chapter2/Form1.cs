dfa7acf838bb8579580eace514e836a8a01102ac
Jin <H.dev@kakao.com>
2021-01-01 13:36:57 +0900

    Commit

---
+++ Chapter2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Firestore;
+
+namespace NetFirebase_Blog
+{
+    public partial class Form1 : Form
+    {
+        FirestoreDb db;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"cs-firebase-blog-firebase-adminsdk-18cwe-b5971a4787.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            db = FirestoreDb.Create("cs-firebase-blog");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Add_Document_with_AutoID();
+        }
+
+
+
+        void Add_Document_with_AutoID()
+        {
+            CollectionReference coll = db.Collection("Add_Document_Width_AutoID");
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"FirestName", "Kim" },
+                {"LastName","Jinwon" },
+                {"PhoneNumber", "010-1234-5678" }
+            };
+            coll.AddAsync(data1);
+            MessageBox.Show("data added successfully");
+        }
+    }
+}
