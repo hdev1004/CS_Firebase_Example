2459a3af796c41ec0fc68be78e95798ae5fc421c
Jin <H.dev@kakao.com>
2021-01-02 02:01:14 +0900

    commit

--- CsFirebaseBlog/Form1.Designer.cs
+++ CsFirebaseBlog/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(6, 20);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 41);
             this.button1.TabIndex = 0;
@@ -46,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Location = new System.Drawing.Point(6, 67);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(153, 41);
             this.button2.TabIndex = 1;
@@ -56,7 +58,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 106);
+            this.button3.Location = new System.Drawing.Point(6, 114);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(153, 41);
             this.button3.TabIndex = 2;
@@ -66,7 +68,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 153);
+            this.button4.Location = new System.Drawing.Point(6, 161);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(153, 41);
             this.button4.TabIndex = 3;
@@ -74,18 +76,29 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 215);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(389, 317);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(643, 255);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +109,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
